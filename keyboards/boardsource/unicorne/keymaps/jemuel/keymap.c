639d3e428707718e8dffd441f5c55a14f23f8d68
Jemuel Balangcod <jemuelb@gmail.com>
2024-11-06 06:52:15 -0800

    Added layer.

--- keyboards/boardsource/unicorne/keymaps/jemuel/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/jemuel/keymap.c
@@ -23,14 +23,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,          KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,      KC_G,          KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,          KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                               KC_LALT, LOWER,     KC_LSFT,       KC_SPC,  RAISE,     KC_LGUI
+                               KC_LALT, LOWER,     KC_LSFT,       KC_SPC,  RAISE,     SDVX
  ),
 
  [_LOWER] = LAYOUT(
      KC_GRV,    S(KC_1), S(KC_2), S(KC_3), S(KC_4),    S(KC_5),       KC_7,    KC_8,    KC_9, KC_MINS,    KC_EQL,    KC_BSLS,
      S(KC_GRV), S(KC_6), S(KC_7), S(KC_8), S(KC_9),    S(KC_0),       KC_4,    KC_5,    KC_6, S(KC_MINS), S(KC_EQL), S(KC_BSLS),
      _______,   _______, KC_LBRC, KC_RBRC, S(KC_LBRC), S(KC_RBRC),    KC_1,    KC_2,    KC_3, _______,    _______,   _______,
-                                  _______, _______,    _______,       KC_0,    _______, KC_LCTL
+                                  _______, _______,    _______,       KC_0,    _______, _______
  ),
 
  [_RAISE] = LAYOUT(
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SDVX] = LAYOUT(
       _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,
+      _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______,       _______, KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, _______,
       _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______,       _______, _______, _______
   )
